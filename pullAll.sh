61cf599d501a1cf0b23b3a6dd80921214eb0d58c
remi128 <qdmaha3t>
2016-04-01 17:13:58 +0200

    adapted to directories

--- pullAll.sh
+++ pullAll.sh
@@ -6,10 +6,10 @@ git -C BtVertxBase pull
 git -C vertx-util pull
 git -C vertx-key-generator pull
 git -C vertx-pojo-mapper pull
-git -C netrelay pull
+git -C NetRelay pull
 git -C NetRelay-Controller pull
 git -C NetRelay-PdfController pull
-git -C NetRelay-Demoproject pull
+git -C NetRelay-DemoProject pull
 
 cd Dev-Helper
 git pull
